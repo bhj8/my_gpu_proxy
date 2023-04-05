89f1375a8716c06a5eec5ec0ccfa3e7778a7c98d
BaoHongJiangGames <baobaoandworld@gmail.com>
2023-04-05 15:13:38 +0800

    1

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ async def add_user(request: AddUserRequest):
         return await handle_proxy_request(request, path, target_url)
 
     route = APIRoute(
-        path=f"/{user_id}/{{path:path}}/",
+        path=f"/{user_id}",#/{{path:path}}/
         endpoint=user_route,
         methods=["GET", "POST", "PUT", "DELETE"],
         # dependencies=[Depends(check_ip)],
